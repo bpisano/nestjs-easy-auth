9502b2683c669dfc4339667db7a40b7b19c1e0b1
Benjamin Pisano <benjamin.pisano@icloud.com>
2023-10-17 11:50:05 +0200

    Added exported types

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { Optional } from '@nestjs/common';
 import { Authenticator } from 'passport';
 import { JwtAuthGuard } from './auth/guards/jwtAuth.guard';
 import { AuthModule } from './auth/modules/auth.module';
 import { MapCredentials } from './auth/types/mapCredentials';
-import { SignInEmailPassword } from './authenticator/signInEmailPassword/signInEmailPassword.authenticator';
+import { MapCredentialsParams } from './auth/types/mapCredentialsParams';
+import {
+  SignInEmailPassword,
+  SignInEmailPasswordInput
+} from './authenticator/signInEmailPassword/signInEmailPassword.authenticator';
 import { AnyCredentialsRepresentation } from './credentials/models/types/anyCredentialsRepresentation';
+import { CredentialsRepresentation } from './credentials/models/types/credentialsRepresentation';
+import { CREDENTIALS_SERVICE } from './credentials/modules/credentials.moduleKeys';
 import { CREDENTIALS_STORAGE } from './credentialsStorage/modules/credentialsStorage.moduleKeys';
 import { CredentialsStorage } from './credentialsStorage/services/credentialsStorage.service';
 import { JWTConfig } from './jwt/models/types/jwtConfig';
 import { AnyUserRepresentation } from './user/models/types/anyUserRepresentation';
+import { UserRepresentation } from './user/models/types/userRepresentation';
+import { USER_SERVICE } from './user/modules/user.moduleKeys';
 import { USER_STORAGE } from './userStorage/modules/userStorage.moduleKeys';
 import { UserStorage } from './userStorage/services/userStorage.service';
 import { DatabaseModelOf } from './utils/types/databaseModelOf';
+import { Optional } from './utils/types/optional';
 import { PromiseOptional } from './utils/types/promiseOptional';
 import { PublicModelOf } from './utils/types/publicModelOf';
 
@@ -20,16 +28,22 @@ export {
   AnyUserRepresentation,
   AuthModule,
   Authenticator,
+  CREDENTIALS_SERVICE,
   CREDENTIALS_STORAGE,
+  CredentialsRepresentation,
   CredentialsStorage,
   DatabaseModelOf,
   JWTConfig,
   JwtAuthGuard,
   MapCredentials,
+  MapCredentialsParams,
   Optional,
   PromiseOptional,
   PublicModelOf,
   SignInEmailPassword,
+  SignInEmailPasswordInput,
+  USER_SERVICE,
   USER_STORAGE,
+  UserRepresentation,
   UserStorage
 };
